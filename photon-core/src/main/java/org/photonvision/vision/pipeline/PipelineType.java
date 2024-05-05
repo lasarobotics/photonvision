5992ef4f13a1eed5218e96b3d7fc8bb515febb01
rachitkakkar <rachitkakkar7@gmail.com>
2024-05-05 17:52:16 -0500

    Work towards adding custom pipeline

--- photon-core/src/main/java/org/photonvision/vision/pipeline/PipelineType.java
+++ photon-core/src/main/java/org/photonvision/vision/pipeline/PipelineType.java
@@ -27,7 +27,8 @@ public enum PipelineType {
     ColoredShape(1, ColoredShapePipeline.class),
     AprilTag(2, AprilTagPipeline.class),
     Aruco(3, ArucoPipeline.class),
-    ObjectDetection(4, ObjectDetectionPipeline.class);
+    ObjectDetection(4, ObjectDetectionPipeline.class),
+    CustomMLDetection(5, CustomMLDetectionPipeline.class);
 
     public final int baseIndex;
     public final Class clazz;
