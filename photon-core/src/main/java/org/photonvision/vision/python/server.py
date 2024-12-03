74f4f285688b7d457363f76abb130ad817d5ab47
David-Dube <117662634+David-Dube@users.noreply.github.com>
2024-12-02 19:24:29 -0600

    Add option to change port in server.py

--- photon-core/src/main/java/org/photonvision/vision/python/server.py
+++ photon-core/src/main/java/org/photonvision/vision/python/server.py
@@ -1,3 +1,10 @@
+import argparse
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('port')
+
+	args = parser.parse_args()
+
 import grpc
 from concurrent import futures
 import photonvision_remote_pipeline_pb2 as prp
@@ -9,25 +16,23 @@ import numpy as np
 
 class ObjectDetectionService(prp_grpc.ObjectDetectionServiceServicer):
 	def ObjectDetection(self, request, context):
-		results = backend.detect(cv2.imdecode(np.fromstring(request.image.image_data, np.uint8), 0))
+		results = backend.detect(cv2.imdecode(np.frombuffer(request.image.image_data, np.uint8), 0))
 		response = prp.ObjectDetectionResponse(results = prp.DetectionResults(
 			id=5,
 			results=[prp.BoundingBox(x=int(r['x']), y=int(r['y']), width=int(r['w']), height=int(r['h']), cls=int(r['class']), confidence=float(r['confidence']))
 					for r in results]
 		))
-		print(int(results[0]['class']))
-		print(results[0]['class'])
 		with open('recv.png', 'wb') as f:
 			f.write(request.image.image_data)
 		return response
 
-def serve():
+def serve(port: int):
 	server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
 	prp_grpc.add_ObjectDetectionServiceServicer_to_server(ObjectDetectionService(), server)
-	server.add_insecure_port('[::]:50051')
+	server.add_insecure_port(f'[::]:{port}')
 	server.start()
-	print("Server is running on port 50051")
+	print(f"Server is running on port {port}")
 	server.wait_for_termination()
 
 if __name__ == '__main__':
-	serve()
+	serve(int(args.port))
